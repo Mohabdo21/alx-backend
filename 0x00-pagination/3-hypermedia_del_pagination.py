302d97e0253c0e2189d0bc4660d9f0f40f4217e0
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-22 03:27:09 +0400

    Try

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -34,7 +34,7 @@ class Server:
                                       for i in range(len(dataset))}
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
         """
         Returns paginated data starting from the given index.
 
