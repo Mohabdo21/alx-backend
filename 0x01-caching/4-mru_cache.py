77a3e4a254c81fbf4129584fc8b6c27533a53475
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-26 11:22:54 +0400

    4. MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" MRUCache module. """
+
+from typing import Any, Optional
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache defines:
+        - caching system using MRU algorithm.
+    """
+
+    def __init__(self) -> None:
+        """Initialize MRU algorithm."""
+        super().__init__()
+        self.keys = []
+
+    def put(self, key: str, item: Any) -> None:
+        """Add an item in the cache using MRU algorithm."""
+        if key and item:
+            self.cache_data[key] = item
+            if key in self.keys:
+                self.keys.remove(key)
+            self.keys.append(key)
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discarded_key = self.keys.pop()
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+
+    def get(self, key: str) -> Optional[Any]:
+        """Get an item by key."""
+        if key in self.cache_data:
+            self.keys.remove(key)
+            self.keys.append(key)
+            return self.cache_data.get(key)
+        return None
