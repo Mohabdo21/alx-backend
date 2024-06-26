549c293acd65d7ed89639dc2ecef4e1e82f6cd25
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-26 11:36:00 +0400

    correct removing the most recently used item

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -20,14 +20,15 @@ class MRUCache(BaseCaching):
     def put(self, key: str, item: Any) -> None:
         """Add an item in the cache using MRU algorithm."""
         if key and item:
-            self.cache_data[key] = item
-            if key in self.keys:
+            if key in self.cache_data:
                 self.keys.remove(key)
-            self.keys.append(key)
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 discarded_key = self.keys.pop()
                 del self.cache_data[discarded_key]
                 print(f"DISCARD: {discarded_key}")
+
+            self.cache_data[key] = item
+            self.keys.append(key)
 
     def get(self, key: str) -> Optional[Any]:
         """Get an item by key."""
