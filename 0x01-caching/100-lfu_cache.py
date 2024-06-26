e666ef0784569e715a757be690cba61f9ad174e3
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-26 12:13:38 +0400

    make the code more readable

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ LFUCache module """
 
-from collections import Counter
+from collections import defaultdict
 from typing import Any, Optional
 
 from base_caching import BaseCaching
@@ -16,39 +16,43 @@ class LFUCache(BaseCaching):
     def __init__(self) -> None:
         """Initialize LFU algorithm."""
         super().__init__()
-        self.keys = []
-        self.counts = Counter()
+        self.usage_count = defaultdict(int)
+        self.key_order = []
 
     def put(self, key: str, item: Any) -> None:
         """Add an item in the cache using LFU algorithm."""
-        if key and item:
-            if key not in self.cache_data:
-                if len(self.keys) >= BaseCaching.MAX_ITEMS:
-                    least_freq = min(self.counts.values())
-                    least_freq_keys = [
-                        k for k, v in self.counts.items() if v == least_freq
-                    ]
-                    if len(least_freq_keys) > 1:
-                        for k in self.keys:
-                            if k in least_freq_keys:
-                                self.keys.remove(k)
-                                del self.counts[k]
-                                del self.cache_data[k]
-                                print(f"DISCARD: {k}")
-                                break
-                    else:
-                        discard = least_freq_keys[0]
-                        self.keys.remove(discard)
-                        del self.counts[discard]
-                        del self.cache_data[discard]
-                        print(f"DISCARD: {discard}")
-                self.keys.append(key)
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
             self.cache_data[key] = item
-            self.counts[key] += 1
+            self.usage_count[key] += 1
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Find the least frequently used item(s)
+                min_cnt = min(self.usage_count.values())
+                least_frequent_keys = [
+                    k for k in self.key_order if self.usage_count[k] == min_cnt
+                ]
+
+                # If there are multiple least frequently used items, use FIFO
+                if len(least_frequent_keys) > 1:
+                    discard_key = least_frequent_keys[0]
+                else:
+                    discard_key = least_frequent_keys[0]
+
+                self.cache_data.pop(discard_key)
+                self.usage_count.pop(discard_key)
+                self.key_order.remove(discard_key)
+                print(f"DISCARD: {discard_key}")
+
+            self.cache_data[key] = item
+            self.usage_count[key] += 1
+            self.key_order.append(key)
 
     def get(self, key: str) -> Optional[Any]:
         """Get an item by key."""
         if key in self.cache_data:
-            self.counts[key] += 1
-            return self.cache_data.get(key)
+            self.usage_count[key] += 1
+            return self.cache_data[key]
         return None
